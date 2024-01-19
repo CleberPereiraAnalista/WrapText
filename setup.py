f0ca99dc59105c2fd2df55eda531d9767b53987e
CleberPereiraAnalista <157152896+CleberPereiraAnalista@users.noreply.github.com>
2024-01-19 18:13:13 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,35 +1,34 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
-    name = 'wraptext',
-    version = '1.0.0',
-    author = 'Cleber Almeida Pereira',
-    author_email = 'cleber.ap.ads@gmail.com',
-    packages = ['wraptext'],
-    description = 'Um simples quebrador de texto para qualquer ocasião escrito em Python.',
-    long_description = 'Um simples quebrador de texto, com função de receber um texto'
-                        + 'e inserir uma quebra de linha a cada 50 caracteres, ou no '
-                        + 'primeiro espaço branco antes do caractere localizado na posição'
-                        + '50, ou conforme o comprimento especificado.',
-    url = 'https://github.com/CleberPereiraAnalista/WrapText.git',
-    project_url = {
-        'Código fonte': 'https://github.com/CleberPereiraAnalista/WrapText.git',
-    },
-    license = 'MIT',
-    keywords = ['quebra-de-linha', 'wrap'],
-    classifiers = [
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: Portuguese (Brazilian)',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'Programming Language :: Python :: 3.13',
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-    ]
-
+    name = "wraptext",
+    version = "1.0.0",
+    description = "Um simples quebrador de texto para qualquer ocasião escrito em Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CleberPereiraAnalista/WrapText.git",
+    author = "Cleber Almeida Pereira",
+    author_email = "cleber.ap.ads@gmail.com",
+    packages = ["wraptext"],
+    classifiers = [  
+    "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: Portuguese (Brazilian)",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    keywords = ["wrap", "quebra-de-linha", "quebra"],
+    python_requires=">=3.7, <4",
 )
